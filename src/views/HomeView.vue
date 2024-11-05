6c67089bc1d0128418f8ceb3ca28592bc7b972eb
asantosmanu <manuelasntos452006@gmail.com>
2024-11-05 11:40:18 -0300

    erro na listagem?

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,32 @@
 <script setup>
 import PatientList from '@/components/PatientList.vue';
+
+import { onMounted } from 'vue';
+import { Passage } from '@passageidentity/passage-js';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+const getUserInfo = async () => {
+  try {
+    const authToken = localStorage.getItem('psg_auth_token');
+    const passage = new Passage("p16xowieXpeF8v7ibn6dKkhH")
+    // const passageUser = new PassageUser(authToken);
+    const user = await passage.currentUser.info()
+    if (user) {
+      await authStore.setToken(authToken);
+    } else {
+      authStore.unsetToken();
+    }
+  } catch (error) {
+    authStore.unsetToken();
+    console.log(error)
+  }
+};
+
+onMounted(() => {
+  getUserInfo();
+});
 </script>
 
 <template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
 <template>
   <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id=p16xowieXpeF8v7ibn6dKkhH
+    ></passage-auth>
+  </div>
 </template>
+
+<style></style>
