21c7c1826a6f6ed82e8132b24e071f88127ff866
joaovnpt <joaovitorguiotti8@gmail.com>
2025-08-07 00:46:49 -0300

    FEAT: Create subpage title component #2

---
+++ src/components/SubpageTitle.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-100 mt-2">
+        <v-text class="text-h5 font-weight-medium">{{ title }}</v-text>
+    </div>
+</template>
+
+<script setup>
+defineProps(['title'])
+</script>
