630111bb54be2271bb3709a2ad0e5feead2cd4f9
joaovnpt <joaovitorguiotti8@gmail.com>
2025-08-28 23:53:37 -0300

    FEAT: Adicionar responsividade de mobile a desktop à página de login #1

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,8 +1,10 @@
 <template>
   <v-img
+    :style="{
+      width: 'clamp(140px, 25vw, 300px)',
+      marginTop: 'clamp(16px, 5vh, 48px)',
+    }"
     src="@/assets/logo-gpter.png"
-    class="mx-auto w-40 mt-15 d-flex flex-0-0"
-  ></v-img>
+    class="flex-0-0"
+  />
 </template>
-
-<script setup></script>
