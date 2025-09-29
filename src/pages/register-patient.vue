3dd9a8ccdddb7fed47120a0d5626118bf617172b
joaovnpt <joaovitorguiotti8@gmail.com>
2025-09-29 12:14:57 -0300

    FEAT: Fazer responsividade completa para página de cadastrar pacientes e deixar dados do PatientForm dinâmicos com vfor #1

--- src/pages/register-patient.vue
+++ src/pages/register-patient.vue
@@ -1,9 +1,13 @@
 <template>
-  <v-container class="h-100">
-    <SubpageTitle title="Cadastrar pacientes" />
+  <v-container
+    class="h-100"
+    :class="{ 'd-flex flex-column align-center justify-center': smAndDown }"
+  >
+    <SubpageTitle v-if="smAndDown" title="Cadastrar pacientes" />
+    <PageTitle v-else justify="justify-center" title="Cadastrar pacientes" />
 
-    <v-row justify="center" align="center" class="h-100">
-      <v-col cols="12" md="8">
+    <v-row justify="center" align="center" class="h-full w-100">
+      <v-col class="pa-0" cols="12" md="8">
         <PatientForm />
       </v-col>
     </v-row>
@@ -13,4 +17,7 @@
 <script setup>
 import SubpageTitle from "@/components/SubpageTitle.vue";
 import PatientForm from "@/components/PatientForm.vue";
+import { useDisplay } from "vuetify";
+
+const { mdAndUp, smAndDown, xs } = useDisplay();
 </script>
