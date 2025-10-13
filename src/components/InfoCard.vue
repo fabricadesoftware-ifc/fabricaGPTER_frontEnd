d5f7c57a7e8541c298db87254c28f38524ab3c7e
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-13 14:17:44 -0300

    FEAT: Criar responsividade completa para o componente InfoCard #1

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -5,19 +5,20 @@
     elevation="2"
     rounded="xl"
   >
-    <v-card-title class="text-h6 font-weight-medium pa-0 mb-4">
+    <v-card-title class="font-weight-medium pa-0" :class="{ 'text-h6 mb-3' : smAndDown, 'text-h5 mb-6': mdAndUp }">
       Informações do paciente
     </v-card-title>
+    <v-divider length="100%" color="grey-darken-4"/>
 
-    <v-card-text class="pa-0">
-      <div class="d-flex flex-column ga-4">
+    <v-card-text :class="['pa-0', {'mt-6': mdAndUp, 'mt-4': smAndDown}]">
+      <div :class="['d-flex flex-column', {'ga-8' : mdAndUp, 'ga-4': smAndDown}]">
         <div v-for="info in infos" :key="info.prop" class="d-flex align-start ga-3">
           <div>
             <component :is="info.icon" :size="20" class="text-blue"/>
           </div>
           <div>
             <span class="font-weight-bold">{{ info.name }}:</span>
-            <span class="ml-1">{{ info.prop }}</span>
+            <span :class="info.name === 'Status' ? 'ml-1 bg-blue-lighten-4 text-blue-darken-4 rounded-xl px-2 py-1' : 'ml-1'">{{ info.prop }}</span>
           </div>
         </div>
       </div>
@@ -25,7 +26,10 @@
   </v-card>
 </template>
 <script setup lang="ts">
+import {useDisplay} from "vuetify";
 import {Clock, Mail, MapPinHouse} from "lucide-vue-next";
+
+const {mdAndUp, smAndDown} = useDisplay()
 
 const props = defineProps({
   address: {
