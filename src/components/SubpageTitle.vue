79a77eddc146133b59b5b844fc2cfb162d4027e8
joaovnpt <joaovitorguiotti8@gmail.com>
2025-10-31 15:01:39 -0300

    FEAT: Add "go back" button on subpage title component with props #1.3

--- src/components/SubpageTitle.vue
+++ src/components/SubpageTitle.vue
@@ -1,9 +1,30 @@
 <template>
-    <div class="w-100">
-        <h2 class="text-h5 font-weight-medium">{{ title }}</h2>
+  <div class="w-100 d-flex align-center ga-2">
+    <div v-if="backButton" @click="goBack" class="d-flex align-center cursor-pointer">
+      <ArrowLeft class="d-flex align-center" />
     </div>
+    <h2 class="text-h5 font-weight-medium">{{ title }}</h2>
+  </div>
 </template>
 
 <script setup>
-defineProps(['title'])
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  backButton: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+import { ArrowLeft } from "lucide-vue-next";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goBack = () => {
+  router.back();
+};
 </script>
